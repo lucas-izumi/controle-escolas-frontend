db0011516e14e1633cb206963253247aa4c5ed68
Lucas Izumi <lucasizumi@gmail.com>
2018-07-12 23:22:16 -0300

    Template HTML

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -11,8 +11,8 @@ gulp.task('deps.js', () => {
     'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
     'node_modules/angular-animate/angular-animate.min.js',
     'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
     'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
     'node_modules/admin-lte/dist/js/app.min.js',
   ])
@@ -25,7 +25,7 @@ gulp.task('deps.css', () => {
   return gulp.src([
     'node_modules/angular-toastr/dist/angular-toastr.min.css',
     'node_modules/font-awesome/css/font-awesome.min.css',
-    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
     'node_modules/admin-lte/dist/css/AdminLTE.min.css',
     'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
   ])
