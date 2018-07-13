1830f1b1f97e92b1ce663dcc3466c8d96a8e2792
Lucas Izumi <lucasizumi@gmail.com>
2018-07-13 02:59:00 -0300

    Adicionando total de alunos na dashboard

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -8,11 +8,16 @@
     const vm = this
     vm.getSummary = function() {
       const url = 'http://localhost:3003/api/escolas/count'
+      const url2 = 'http://localhost:3003/api/escolas/turmasCount'
       //extrai a variavel "value" do objeto, nesse caso, o numero de escolas
       //a variavel ira entrar com valor padrao de 0, caso um valor nao seja encontrado
       $http.get(url).then(function(response){
         var data = response.data
         vm.numeroEscolas = data.value
+      })
+      $http.get(url2).then(function(response){
+        var data = response.data
+        vm.numeroTurmas = data.value
       })
     }
 
