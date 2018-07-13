63756bbec424aa317c40bece0bc5d39079c3b1bf
Lucas Izumi <lucasizumi@gmail.com>
2018-07-12 22:06:53 -0300

    Adicionado app

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -16,9 +16,9 @@ gulp.task('deps.js', () => {
     'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
     'node_modules/admin-lte/dist/js/app.min.js',
   ])
-  .pipe(uglify());
-  .pipe(concat('deps.min.js'));
-  .pipe(gulp.dest('public/assets/js'));
+  .pipe(uglify())
+  .pipe(concat('deps.min.js'))
+  .pipe(gulp.dest('public/assets/js'))
 })
 
 gulp.task('deps.css', () => {
@@ -29,9 +29,9 @@ gulp.task('deps.css', () => {
     'node_modules/admin-lte/dist/css/AdminLTE.min.css',
     'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
   ])
-  .pipe(uglifycss({ "uglyComments": true }));
-  .pipe(concat('deps.min.css'));
-  .pipe(gulp.dest('public/assets/css'));
+  .pipe(uglifycss({ "uglyComments": true }))
+  .pipe(concat('deps.min.css'))
+  .pipe(gulp.dest('public/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
@@ -39,5 +39,5 @@ gulp.task('deps.fonts', () => {
     'node_modules/font-awesome/fonts/*.*',
     'node_modules/admin-lte/bootstrap/fonts/*.*'
   ])
-  .pipe(gulp.dest('public/assets/fonts'));
+  .pipe(gulp.dest('public/assets/fonts'))
 })
