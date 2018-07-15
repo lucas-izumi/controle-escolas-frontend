4f03edd455a4acda40edd9685690cd980130ac84
Lucas Izumi <lucasizumi@gmail.com>
2018-07-15 15:58:35 -0300

    adicionando factory de mensagens de erro]

--- app/cadastroEscolas/cadastroEscolasController.js
+++ app/cadastroEscolas/cadastroEscolasController.js
@@ -1,19 +1,20 @@
 (function() {
   angular.module('CadastroEscolas').controller('CadastroEscolasCtrl', [
     '$http',
+    'msgs',
     CadastroEscolasController
   ])
 
-  function CadastroEscolasController($http) {
+  function CadastroEscolasController($http, msgs) {
     const vm = this
 
     vm.create = function() {
       const url = 'http://localhost:3003/api/escolas'
       $http.post(url, vm.cadastroEscolas).then(function(response) {
         vm.cadastroEscolas = {}
-        console.log('Adicionado com sucesso!')
-      }, function(error) {
-        console.log('Registro não inserido!')
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response) {
+        msgs.addError(response.data.errors)
       })
     }
   }
