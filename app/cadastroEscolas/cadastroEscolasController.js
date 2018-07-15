cddb0093914dc677ff9d8901a872a64aa8486adf
Lucas Izumi <lucasizumi@gmail.com>
2018-07-15 16:57:43 -0300

    melhorando navegação abas

--- app/cadastroEscolas/cadastroEscolasController.js
+++ app/cadastroEscolas/cadastroEscolasController.js
@@ -2,10 +2,11 @@
   angular.module('CadastroEscolas').controller('CadastroEscolasCtrl', [
     '$http',
     'msgs',
+    'tabs',
     CadastroEscolasController
   ])
 
-  function CadastroEscolasController($http, msgs) {
+  function CadastroEscolasController($http, msgs, tabs) {
     const vm = this
     const url = 'http://localhost:3003/api/escolas'
 
@@ -13,6 +14,7 @@
       $http.get(url).then(function(response) {
         vm.cadastroEscolas = {}
         vm.escola = response.data
+        tabs.show(vm, {tabList: true, tabCreate: true})
       })
     }
 
@@ -23,6 +25,17 @@
       }).catch(function(response) {
         msgs.addError(response.data.errors)
       })
+    }
+
+    //pega o registro atual
+    vm.showTabUpdate = function(cadastroEscolas) {
+      vm.cadastroEscolas = cadastroEscolas
+      tabs.show(vm, {tabUpdate: true})
+    }
+
+    vm.showTabDelete = function(cadastroEscolas) {
+      vm.cadastroEscolas = cadastroEscolas
+      tabs.show(vm, {tabDelete: true})
     }
 
     vm.refresh()
