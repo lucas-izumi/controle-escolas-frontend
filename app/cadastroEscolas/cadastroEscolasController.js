f22b569c78e671a8bc3337af10e4ff0c13fa3401
Lucas Izumi <lucasizumi@gmail.com>
2018-07-15 16:17:59 -0300

    Listagem de dados

--- app/cadastroEscolas/cadastroEscolasController.js
+++ app/cadastroEscolas/cadastroEscolasController.js
@@ -7,15 +7,24 @@
 
   function CadastroEscolasController($http, msgs) {
     const vm = this
+    const url = 'http://localhost:3003/api/escolas'
+
+    vm.refresh = function() {
+      $http.get(url).then(function(response) {
+        vm.cadastroEscolas = {}
+        vm.escola = response.data
+      })
+    }
 
     vm.create = function() {
-      const url = 'http://localhost:3003/api/escolas'
       $http.post(url, vm.cadastroEscolas).then(function(response) {
-        vm.cadastroEscolas = {}
+        vm.refresh()
         msgs.addSuccess('Operação realizada com sucesso!')
       }).catch(function(response) {
         msgs.addError(response.data.errors)
       })
     }
+
+    vm.refresh()
   }
 })()
